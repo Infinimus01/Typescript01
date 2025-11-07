ebd8f272fded613e920e76d528d319079ab550e8
AMLENDU PANDEY <gaurau1611@gmail.com>
2025-11-07 17:18:59 +0530

    initial commit

---
+++ src/day1.ts
@@ -0,0 +1,21 @@
+// 1. type annotations (string, boolean, number, any, undefined , unknown, null)
+// 2. array and tuples 
+// 3. object type (interface , type)
+// 4. funtion type(parameter, return)
+
+
+let username: string = "amlendu";
+let age: number = 30;
+let isGraduated: boolean = true;
+let skills : string[] = ["React", "Node", "Typescript"];
+
+let userTuple: [string, number, boolean]=["amlendu", 30, true];
+
+console.log(username, age, isGraduated,  userTuple, skills);
+
+
+
+
+
+
+
